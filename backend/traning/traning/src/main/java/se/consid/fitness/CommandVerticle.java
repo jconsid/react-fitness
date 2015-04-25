a9cb85b1b172f0f26972fa10a7cd5af6a28bee72
Anders Johansson <anders.johansson121@gmail.com>
2015-04-25 16:39:03 +0200

    Lagt till command gateway.

--- backend/traning/traning/src/main/java/se/consid/fitness/CommandVerticle.java
+++ backend/traning/traning/src/main/java/se/consid/fitness/CommandVerticle.java
@@ -3,7 +3,9 @@
  */
 package se.consid.fitness;
 
-import org.vertx.java.core.http.HttpServer;
+import org.vertx.java.core.eventbus.EventBus;
+import org.vertx.java.core.http.RouteMatcher;
+import org.vertx.java.core.json.JsonObject;
 import org.vertx.java.core.logging.Logger;
 import org.vertx.java.platform.Verticle;
 
@@ -14,15 +16,28 @@ public class CommandVerticle extends Verticle {
 	@Override
 	public void start() {
 		final Logger log = container.logger();
+		final EventBus eb = vertx.eventBus();
 
-		final HttpServer server = vertx.createHttpServer();
+		final RouteMatcher routeMatcher = new RouteMatcher();
+		routeMatcher.post("/requests", req -> {
+			req.bodyHandler(buffer -> {
+				final String json = buffer.getString(0, buffer.length());
+				final JsonObject request = new JsonObject(json);
 
-		server.requestHandler(req -> {
-			log.info("A request has arrived on the server!");
-			req.response().end();
+				switch (request.getString("typ")) {
+				case "registreraTraningstillfalle":
+					eb.send("anvandareCommandHandler", request);
+					break;
+
+				default:
+					break;
+				}
+			});
+
+			req.response().setStatusCode(402).end();
 		});
 
-		server.listen(8000);
+		vertx.createHttpServer().requestHandler(routeMatcher).listen(8000);
 
 		log.info("Command server started.");
 	}
